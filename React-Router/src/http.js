ccaf4e5fe54c07ec5bd968705ac8e737a758e39a
leen <leenhija171@gmail.com>
2024-10-04 00:02:00 +0300

    deleted a file

--- React-Router/src/http.js
+++
@@ -1,8 +0,0 @@
-export const fetchEvents=async()=>{
-    const response=await fetch('http://localhost:8080/events');
-    const EventsData=await response.json();
-    if(!response.ok){
-        throw new Error('Failed to fetch Events Data')
-    }
-    return EventsData.events;
-}
